f982b1bec32dd535f672b9fbac00c12763a1a41f
tramanh <nnta.3498@gmail.com>
2020-07-17 11:46:40 +0700

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,11 @@ module SessionsHelper
   end
 
   def logged_in?
-    !current_user.present?
+    current_user.present?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,8 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
   fixtures :all
   include ApplicationHelper
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
