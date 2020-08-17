9ab89f5c45a6a29cfea6834b54f2bed88b7995e4
tramanh <nnta.3498@gmail.com>
2020-08-17 17:12:19 +0700

    doing 14.3.2 A first feed implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :  BCrypt::Engine.cost
@@ -68,6 +72,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
