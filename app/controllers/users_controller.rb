082d91fb772325d6b077eeaa205d3aa77d1c0639
twenty-one-21 <nnta.3498@gmail.com>
2020-08-20 18:56:51 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,16 +48,14 @@ class UsersController < ApplicationController
 
   def following
     @title = "Following"
-    @user = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
-    render "show_follow"
+    render :show_follow
   end
 
   def followers
     @title = "Followers"
-    @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render "show_follow"
+    render :show_follow
   end
 
   private
