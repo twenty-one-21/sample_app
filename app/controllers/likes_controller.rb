657cdedebee97633fa3946d30567e6f5072af506
twenty-one-21 <nnta.3498@gmail.com>
2020-10-18 19:00:11 +0700

    unlike draft ver

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,26 @@ class LikesController < ApplicationController
   before_action :check_logged_in_user, only: [:create]
 
   def create
-    @post = Post.find_by(params[:post_id])
-    @user = User.find(params[:user_id])
-    @post.likes.new(user_id: current_user.id)
-    if @like.save
+    @micropost = Micropost.find_by(params[:micropost_id])
+    @micropost.likes.new(user_id: current_user.id)
+    @like = @micropost.likes.new(user_id: current_user.id)
+    if @like.save!
       flash[:success] = "Like created!"
     else
       flash[:danger] = "Failed"
     end
     redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @like = Like.find_by(params[:like_id])
+    @like.destroy
+    if current_user.destroy(@like)
+      flash[:success] = "Unliked"
+    else
+      flash[:danger] = "Failed"
+    end
+    redirect_to request.referrer || root_url
   end
 
   private
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
     belongs_to :user
-    belongs_to :micropost
+    belongs_to :micropost, foreign_key: :post_id
 
     validates :user_id, presence: true
     validates :post_id, presence: true
